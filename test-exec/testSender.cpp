1748135e3e8c356729f94bd9898bd91f9923fd82
selvam-manojkumar <77610809+selvam-manojkumar@users.noreply.github.com>
2021-05-28 20:17:23 +0530

    Add files via upload

---
+++ test-exec/testSender.cpp
@@ -0,0 +1,29 @@
+#include "stdafx.h"
+
+
+#include<iostream>
+#include <stdlib.h>
+#include <sstream>
+#include "bms_sender.h"
+#include "test\catch.hpp"
+
+
+TEST_CASE("validate input data") {
+	class BMS_Data_generator * obj = new class BMS_Data_generator;
+	BMS_DATA testData;
+	testData = obj->pushToTest(obj->Output_JSON_Data());
+	REQUIRE(BMS_Data_generator::Validate_Range(testData.SOC, MIN_SOC, MAX_SOC));
+	REQUIRE(BMS_Data_generator::Validate_Range(testData.temperature, MIN_TEMP, MAX_TEMP));
+}
+
+TEST_CASE("data generation test") {
+
+	class BMS_Data_generator * obj = new class BMS_Data_generator;
+	BMS_DATA testData;
+	for (int noOfIter = 0; noOfIter < 50; noOfIter++) 
+	{
+		testData = obj->pushToTest(obj->Output_JSON_Data());
+		REQUIRE(BMS_Data_generator::Validate_Range(testData.SOC, MIN_SOC, MAX_SOC));
+		REQUIRE(BMS_Data_generator::Validate_Range(testData.temperature, MIN_TEMP, MAX_TEMP));
+	}
+}
