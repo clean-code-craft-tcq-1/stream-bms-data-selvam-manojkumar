78912e938e77423dc97b4aad9199fc20bf868b8e
selvam-manojkumar <77610809+selvam-manojkumar@users.noreply.github.com>
2021-05-29 15:32:41 +0530

    Update testSender.cpp

--- test-exec/testSender.cpp
+++ test-exec/testSender.cpp
@@ -6,21 +6,21 @@
 
 
 TEST_CASE("validate input data") {
-	class BMS_Data_generator * obj = new class BMS_Data_generator;
+	class BMS_generator_tester * tester_obj = new class BMS_generator_tester;
 	BMS_DATA testData;
-	testData = obj->pushToTest(obj->Output_JSON_Data());
-	REQUIRE(BMS_Data_generator::Validate_Range(testData.SOC, MIN_SOC, MAX_SOC));
-	REQUIRE(BMS_Data_generator::Validate_Range(testData.temperature, MIN_TEMP, MAX_TEMP));
+	testData = tester_obj->pushToTest(obj->Output_JSON_Data());
+	REQUIRE(tester_obj->Validate_Range(testData.SOC, MIN_SOC, MAX_SOC));
+	REQUIRE(tester_obj->BMS_generator_tester::Validate_Range(testData.temperature, MIN_TEMP, MAX_TEMP));
 }
 
 TEST_CASE("data generation test") {
 
-	class BMS_Data_generator * obj = new class BMS_Data_generator;
+	class BMS_generator_tester * tester_obj = new class BMS_generator_tester;
 	BMS_DATA testData;
 	for (int noOfIter = 0; noOfIter < 50; noOfIter++) 
 	{
-		testData = obj->pushToTest(obj->Output_JSON_Data());
-		REQUIRE(BMS_Data_generator::Validate_Range(testData.SOC, MIN_SOC, MAX_SOC));
-		REQUIRE(BMS_Data_generator::Validate_Range(testData.temperature, MIN_TEMP, MAX_TEMP));
+		testData = tester_obj->pushToTest(obj->Output_JSON_Data());
+		REQUIRE(tester_obj->Validate_Range(testData.SOC, MIN_SOC, MAX_SOC));
+		REQUIRE(tester_obj->Validate_Range(testData.temperature, MIN_TEMP, MAX_TEMP));
 	}
 }
