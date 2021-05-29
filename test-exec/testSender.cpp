439d843114f6665629e8c799d161a9e52d7468f3
selvam-manojkumar <77610809+selvam-manojkumar@users.noreply.github.com>
2021-05-29 15:56:07 +0530

    Update testSender.cpp

--- test-exec/testSender.cpp
+++ test-exec/testSender.cpp
@@ -10,7 +10,7 @@ TEST_CASE("data generation test") {
 	BMS_DATA testData;
 	for (int noOfIter = 0; noOfIter < 50; noOfIter++) 
 	{
-		testData = tester_obj->pushToTest(obj->Output_JSON_Data());
+		testData = tester_obj->pushToTest(tester_obj->Output_JSON_Data());
 		REQUIRE(tester_obj->Validate_Range(testData.SOC, MIN_SOC, MAX_SOC));
 		REQUIRE(tester_obj->Validate_Range(testData.temperature, MIN_TEMP, MAX_TEMP));
 	}
