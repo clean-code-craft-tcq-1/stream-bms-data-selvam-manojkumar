9fbd90e21922babb979bdfab4adf1b58d49abf80
selvam-manojkumar <77610809+selvam-manojkumar@users.noreply.github.com>
2021-05-29 15:44:27 +0530

    Update testSender.cpp

--- test-exec/testSender.cpp
+++ test-exec/testSender.cpp
@@ -4,15 +4,6 @@
 #include "../sender/bms_sender.h"
 #include "catch.hpp"
 
-
-TEST_CASE("validate input data") {
-	class BMS_generator_tester * tester_obj = new class BMS_generator_tester;
-	BMS_DATA testData;
-	testData = tester_obj->pushToTest(obj->Output_JSON_Data());
-	REQUIRE(tester_obj->Validate_Range(testData.SOC, MIN_SOC, MAX_SOC));
-	REQUIRE(tester_obj->Validate_Range(testData.temperature, MIN_TEMP, MAX_TEMP));
-}
-
 TEST_CASE("data generation test") {
 
 	class BMS_generator_tester * tester_obj = new class BMS_generator_tester;
